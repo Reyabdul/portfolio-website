11fd01f1d6c550177d87822b76d7c99b3d30daa0
Reyabdul <reyabdul@gmail.com>
2025-04-16 16:36:12 +0900

    add Tacnstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './global.css'
 import App from './App.tsx'
+//TANSTACK QUERY
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+//STYLES
+import './global.css'
+
+//Tanstack Query
+  //Create a client
+  const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
 )
